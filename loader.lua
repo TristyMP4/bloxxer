b4cde81825faaeb3b9759a687bc13c1b79040131
TristyMP4 <support@tristymp4.xyz>
2024-12-24 22:55:11 +0000

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,51 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "BloxxerHub",
+    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    LoadingTitle = "Initialising..",
+    LoadingSubtitle = "by traz",
+    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+ 
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+ 
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "bloxxerhub"
+    },
+ 
+    Discord = {
+       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+ 
+    KeySystem = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "BloxxerHub",
+       Subtitle = "Key System",
+       Note = "Use the key you were provided.", -- Use this to tell the user how to get a key
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"bloxxer101","coolkidd","papa noel"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
+
+ local ScriptsTab = Window:CreateTab("Scripts", "scroll") -- Title, Image
+ local UniversalScripts = ScriptsTab:CreateSection("Universal")
+
+ local flingButton = UniversalScripts:CreateButton({
+   Name = "Fling GUI",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/hellohellohell012321/KAWAII-FREAKY-FLING/main/kawaii_freaky_fling.lua",true))()
+   end,
+})
+
+print("a")
+
+task.wait(50)
+
+Rayfield:Destroy()
