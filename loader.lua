2440cabf6b24cac1b99d544954d1f93480449778
TristyMP4 <tristymp4@gmail.com>
2024-12-25 01:20:06 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local notifDuration = 2
+local loadedKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TristyMP4/bloxxer/refs/heads/main/keys"))()
 
 function universal()
    local Window = Rayfield:CreateWindow({
@@ -32,8 +33,8 @@ function universal()
          Note = "Use the key you were provided.", -- Use this to tell the user how to get a key
          FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
          SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-         GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-         Key = "https://raw.githubusercontent.com/TristyMP4/bloxxer/refs/heads/main/keys" -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+         Key = loadedKeys -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
       }
    })
   
