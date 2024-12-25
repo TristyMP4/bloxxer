9dbbd27901044191a89f45ad32bb6908d73f585c
TristyMP4 <tristymp4@gmail.com>
2024-12-25 01:34:35 +0000

    Update luckyblockbattlegrounds.lua

--- luckyblockbattlegrounds.lua
+++ luckyblockbattlegrounds.lua
@@ -4,7 +4,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local notifDuration = 2
 local loadedKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TristyMP4/bloxxer/refs/heads/main/keys.lua"))()
-print(tostring(loadedKeys))
 
 local Window = Rayfield:CreateWindow({
     Name = "BloxxerHub",
