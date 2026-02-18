f4bce0f8210b764183bbd0d9645b43879e2c22da
TristyMP4 <tristymp4@gmail.com>
2026-02-18 19:20:59 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 106931261124996 then
+if game.PlaceId == 106931261124996 or game.PlaceId == 662417684 then
    print("lucky block battlegrounds")
    loadstring(game:HttpGet('https://github.com/TristyMP4/bloxxer/raw/refs/heads/main/luckyblockbattlegrounds.lua'))()
 elseif game.PlaceId == 286090429 then
@@ -14,6 +14,7 @@ else
    print("universal")
    loadstring(game:HttpGet("https://raw.githubusercontent.com/TristyMP4/bloxxer/refs/heads/main/universal.lua"))()
 end
+
 
 
 
